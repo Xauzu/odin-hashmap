afd34b2d73f71b039962daa9f6a0e70c0cc8f538
Xau <23320206+Xauzu@users.noreply.github.com>
2024-01-29 17:45:20 -0500

    Add hash function from Odin

---
+++ hashmap.js
@@ -0,0 +1,17 @@
+class HashMap {
+    constructor() {
+        this.buckets = [];
+    }
+
+    // From Odin
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+
+        return hashCode;
+    }
+}
